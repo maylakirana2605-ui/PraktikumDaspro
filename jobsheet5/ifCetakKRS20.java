d1cfd6926994e652e38a2342c338f43761478706
maylakirana2605-ui <maylakirana2605@gmail.com>
2025-10-01 22:34:17 +0700

    Modifikasi percobaan 1

--- jobsheet5/ifCetakKRS20.java
+++ jobsheet5/ifCetakKRS20.java
@@ -11,7 +11,10 @@ public class ifCetakKRS20 {
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else {
+             System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
         }
+
 
     }
 }
