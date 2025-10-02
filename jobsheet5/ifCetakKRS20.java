a6ed979da24c6acabd9247395b51e2dd5194327d
maylakirana2605-ui <maylakirana2605@gmail.com>
2025-10-02 10:37:44 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS20.java
+++ jobsheet5/ifCetakKRS20.java
@@ -2,19 +2,29 @@ import java.util.Scanner;
 
 public class ifCetakKRS20 {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
+
+        String statusPembayaran;
+        String instruksiSelanjutnya;
 
         System.out.println("--- Cetak KRS SIAKAD ---");
         System.out.print("Apakah UKT sudah lunas? (true/false): "); 
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        } else {
-             System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
+        statusPembayaran = uktLunas ? 
+            "Pembayaran UKT terverifikasi" : 
+            "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
 
+        instruksiSelanjutnya = uktLunas ?
+            "Silahkan cetak KRS dan minta tanda tangan DPA" :
+            "";
+
+        System.out.println(statusPembayaran);
+        
+        if (!instruksiSelanjutnya.isEmpty()) { 
+             System.out.println(instruksiSelanjutnya);
+        }
 
     }
 }
